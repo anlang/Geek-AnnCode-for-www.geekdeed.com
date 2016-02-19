74867c5a819729054c851fcacbfe594259ab9935
Jiankian <luozhengan@vip.qq.com>
2016-02-19 10:16:19 +0800

    添加二维码生成和识别
    支持生成二维码，支持打开摄像头扫描，支持相册扫描

--- app/src/main/java/cn/anline/geek/HomeActivity.java
+++ app/src/main/java/cn/anline/geek/HomeActivity.java
@@ -1,7 +1,12 @@
 package cn.anline.geek;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import cn.anline.qrcode.QRcodeIndexActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        TextView textView1 = (TextView)findViewById(R.id.text3);
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goQrHome = new Intent(HomeActivity.this, QRcodeIndexActivity.class);
+                startActivity(goQrHome);
+            }
+        });
     }
 }
